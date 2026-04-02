dff355bcaf98084721fca9275883bbdbcc9e1fd6
Suhayb <suhayb.yunus.314@gmail.com>
2026-04-02 09:37:54 +0100

    feat: add TestimonialCard component consuming testimonials data

--- components/ui/TestimonialCard.tsx
+++ components/ui/TestimonialCard.tsx
@@ -1,1 +1,12 @@
-export default function TestimonialCard() { return <div /> }
+import type { Testimonial } from '@/data/testimonials'
+
+export default function TestimonialCard({ testimonial }: { testimonial: Testimonial }) {
+  return (
+    <div className="tcard reveal">
+      <span className="tm-mark">"</span>
+      <p className="tm-text">{testimonial.quote}</p>
+      <div className="tm-name">{testimonial.name}</div>
+      <div className="tm-title">{testimonial.title}</div>
+    </div>
+  )
+}
