9be743365d4e6936c5a6160fdf7eb790ce4330ca
Suhayb <suhayb.yunus.314@gmail.com>
2026-04-02 08:22:46 +0100

    feat: wire Marquee and Manifesto into page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Nav from "@/components/Nav"
 import Hero from "@/components/Hero"
+import Marquee from "@/components/Marquee"
+import Manifesto from "@/components/Manifesto"
 
 export default function Home() {
   return (
     <main>
       <Nav />
       <Hero />
-      {/* Marquee */}
-      {/* Manifesto */}
+      <Marquee />
+      <Manifesto />
       {/* Services */}
       {/* AddOns */}
       {/* About */}
