2ff04937871d505e6883dbf3bf26218fe18ae456
Suhayb <suhayb.yunus.314@gmail.com>
2026-04-02 09:20:07 +0100

    feat: add AddOnItem component consuming addons data

--- components/ui/AddOnItem.tsx
+++ components/ui/AddOnItem.tsx
@@ -1,1 +1,11 @@
-export default function TestimonialCard() { return <div /> }
+import type { AddOn } from '@/data/addons'
+
+export default function AddOnItem({ addon }: { addon: AddOn }) {
+  return (
+    <div className="ao-item reveal">
+      <span className="ao-icon">{addon.icon}</span>
+      <div className="ao-name">{addon.name}</div>
+      <div className="ao-desc">{addon.description}</div>
+    </div>
+  )
+}
